886ca2c7aa7f6ee01a6ac1e4052860afe2671b58
Sujal Shakya <sujalshakya387@gmail.com>
2024-09-09 10:21:07 +0545

    Fix: Abstract and concrete classes for fetch user repository

--- lib/base/provider/user_provider.dart
+++ lib/base/provider/user_provider.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp/base/models/user.dart';
-import 'package:whatsapp/base/repository/fetch_repository.dart';
+import 'package:whatsapp/base/repository/fetch_user/fetch_repository_implementation.dart';
 
 class UserProvider extends ChangeNotifier {
   late UserData users = UserData(data: []);
   bool fetch = true;
-
+  final fetchRepo = FetchRepositoryImplementation();
   void fetchUsers() async {
     if (fetch == true) {
       try {
-        final fetchedUsers = await FetchRepository.fetchUsersToRepository();
+        final fetchedUsers = await fetchRepo.fetchUsersToRepository();
         users = fetchedUsers;
         fetch = false;
         notifyListeners();
--- lib/base/repository/fetch_user/fetch_repository_implementation.dart
+++ lib/base/repository/fetch_user/fetch_repository_implementation.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp/base/models/user.dart';
+import 'package:whatsapp/base/repository/fetch_user/fetch_repository.dart';
 import 'package:whatsapp/base/service/fetch_service.dart';
 
 /// Convert Api data to list of user datas.
 
-abstract class FetchRepository {
-  static fetchUsersToRepository() async {
+interface class FetchRepositoryImplementation implements FetchRepository {
+  @override
+  fetchUsersToRepository() async {
     try {
       final response = await FetchService.fetchUsersApiCall();
       return UserData.fromJson(response.data);
